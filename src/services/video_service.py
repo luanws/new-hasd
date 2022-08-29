915f8463433251f06c9737021511fdca7daf9ee5
Luan Willig Silveira <luan.w.silveira@gmail.com>
2022-08-29 15:21:10 -0300

    Update video_service.py

--- src/services/video_service.py
+++ src/services/video_service.py
@@ -1,4 +1,5 @@
 import os
+import re
 from typing import List
 
 from src.utils.string import normalize
@@ -11,16 +12,25 @@ class VideoService:
 
     def __init__(self, directory: str) -> None:
         self.directory = os.path.join(os.getcwd(), directory)
-        self.videos = self.get_videos()
+        self.__update_videos()
 
     def remove_file_extension(self, file_name: str) -> str:
         return ''.join(file_name.split('.')[:-1])
 
-    def get_videos(self) -> List[str]:
+    def __update_videos(self) -> List[str]:
         self.videos = os.listdir(self.directory)
         self.videos = [self.remove_file_extension(v) for v in self.videos]
-        self.videos.sort()
+        self.videos.sort(key=self.__get_sort_videos_key())
         return self.videos
+
+    def __get_sort_videos_key(self):
+        size = len(self.videos)
+
+        def sort_videos_key(video: str) -> int:
+            match = re.search(r'^(\d+)', video)
+            video_number = match.group(0) if match else size + 1
+            return int(video_number)
+        return sort_videos_key
 
     def search_videos(self, search_text: str) -> List[str]:
         filtered_videos = [video for video in self.videos if normalize(search_text.lower()) in normalize(video.lower())]
